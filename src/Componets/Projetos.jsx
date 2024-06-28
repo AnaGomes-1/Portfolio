c70fc69f411b0f067e1ba39404220e0c880241c1
anabeatrizgomes3@gmail.com <anabeatrizgomes3@gmail.com>
2024-06-28 00:40:49 -0300

    alterações na aba de projetos(não finalizado)

--- src/Componets/Projetos.jsx
+++ src/Componets/Projetos.jsx
@@ -1,87 +1,59 @@
-import React from "react"
-import Fantastika from "./Imagens/fantastika.png"
-import Witcher from "./Imagens/thewitcher.png"
-import Primeiro from "./Imagens/primeiro_projeto.png"
-import Cartoon from "./Imagens/cartoon.png"
-import * as S from "./Style.jsx"
-import Carousel from "nuka-carousel";
+import React from "react";
+import * as S from "./Style";
 
-function Projetos(){
+import Loja from "./Imagens/lojinha.png";
+import Witcher from "./Imagens/thewitcher.png";
+import Cartoon from "./Imagens/lojinha.png";
+//import Loja from "./Imagens/lojinha.png";
+//import Loja from "./Imagens/lojinha.png";
+//import Loja from "./Imagens/lojinha.png";
 
-    const imagens = [
-        {imagem: Fantastika, titulo: "Fantastika", link: "https://www.bing.com/", descricao:"HTML, CSS, Responsivo"},
-        {imagem: Witcher, titulo: "The Witcher", link: "https://www.bing.com/", descricao:"react, map, styled-components, useState, useEffect, filter"},
-        {imagem: Primeiro, titulo: "Primeiro Projeto", link: "https://www.bing.com/", descricao:"Lógica - JS"},
-        {imagem: Cartoon, titulo: "Cartoon Network", link: "https://www.bing.com/", descricao:"html, css, position"}
-    ]
+function Projetos() {
+  return (
+    <S.SectionProjetos>
+      <S.Projetos>
+        <img src={Loja} alt="Loja da Beca" />
+        <h2>Loja da Bia</h2>
+        <h3>HTML, CSS, Javascript BootStrap</h3>
+        <a 
+          href="" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Visite a Loja
+        </a>
+      </S.Projetos>
 
-    const Carrosel = {
-        width: "99vw",
-        heigth: "80vh",
-        backgroundColor: "black",
-    };
+        <S.Projetos>
+          <img src={Witcher} alt="" />
+          <h2>The Witcher</h2>
+          <h3>HTML, CSS</h3>
+          <a href="" target="_blank" rel="noopener noreferrer">Visite o site</a>
+        </S.Projetos>
 
-    const params = {
-        wrapAround: true,
-        animation: 'zoom',
-        slidesToShow: 1, //de 3 em 3 imagens
-        style: Carrosel,
-        autoplay: true, //passar sozinho
-        autoplayInterval: 3000, //passar de 3 em 3 seguntos automático
-        cellAlign: 'left',
-        swiping: true, //arrastar o carousel,
-        zoomScale: 0.85,
-        className: 'teste',
-    
-        defaultControlsConfig: {
-    
-          nextButtonText: '»',
-          nextButtonStyle: {
-            //position:"relative",
-            // top:"-6.5vh",
-            marginRight: '3.8vw',
-            // color: "#ffffff",
-            color: 'red',
-            fontSize: '192px',
-            backgroundColor: 'transparent',
-            width: '2vw',
-            // height: '8vh',
-            fontFamily: "Inconsolata",
-          },
-    
-          prevButtonText: '«',
-          prevButtonStyle: {
-            // position: "relative",
-            // top:"-6.5vh",
-            // color: "#ffffff",
-            color: 'red',
-            fontSize: '192px',
-            backgroundColor: 'transparent',
-            width: '2vw',
-            // height: '8vh', 
-            fontFamily: "Inconsolata",
-            Dots: "red",
-            
-          },
-        }
-    
-      }
+          <S.Projetos>
+            <img src={Cartoon} alt="" />
+            <h2>Cartoon Network</h2>
+            <h3>HTML, CSS E JAVASCRIPT</h3>
+            <a href="" target="_blank" rel="noopener noreferrer">Visite o codigo</a>
+          </S.Projetos>
 
+          {/* <S.Projetos>
+            <img src={TheWitcher} alt="" />
+            <h2>The Witcher</h2>
+            <h3>HTML, CSS</h3>
+            <a href="" target="_blank" rel="noopener noreferrer">Visite o site</a>
+          </S.Projetos>
 
-    return(
-       <>
-            <Carousel {...params}>
-                {desafios.map((item) =>(
-                    <div>
-                    <a href={item.link} target="_blank">   <img src={item.imagem} alt={item.titulo} /> </a>
-                    <h2>{item.titulo}</h2>
-                    <h2>{item.descricao}</h2>
-                </div>
-                ))}
-            
-            </Carousel>
-       </>
-    )
+          <S.Projetos>
+            <img src={Disney} alt="" />
+            <h2>Disney</h2>
+            <h3>HTML, CSS</h3>
+            <a href="" target="_blank" rel="noopener noreferrer">Visite o site</a>
+          </S.Projetos> */}
+
+    </S.SectionProjetos>
+  );
 }
 
-export default Projetos
+export default Projetos;
